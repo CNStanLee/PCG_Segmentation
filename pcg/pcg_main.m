41e9088f28d3374f7fa9770d4797117c30aa65bf
Changhong <15284244183@163.com>
2024-11-20 06:16:12 +0000

    update

--- pcg/pcg_main.m
+++ pcg/pcg_main.m
@@ -93,34 +93,34 @@ PCG = signal_filtered;
 % 3.2 use HMM to segment the signal
 % -------------------------------------------------------------------------
 
-% % Load the default options:
+% Load the default options:
 
-% addpath('segmentation/springerHMM');
-% springer_options = default_Springer_HSMM_options;
-% load('example_data.mat');
+addpath('segmentation/springerHMM');
+springer_options = default_Springer_HSMM_options;
+load('example_data.mat');
 
-% % Split the data into train and test sets:
-% % Select the first 5 recordings for training and the sixth for testing:
-% train_recordings = example_data.example_audio_data([1:5]);
-% train_annotations = example_data.example_annotations([1:5],:);
+% Split the data into train and test sets:
+% Select the first 5 recordings for training and the sixth for testing:
+train_recordings = example_data.example_audio_data([1:5]);
+train_annotations = example_data.example_annotations([1:5],:);
 
-% % test_recordings = example_data.example_audio_data(6);
-% % test_annotations = example_data.example_annotations(6,:);
+% test_recordings = example_data.example_audio_data(6);
+% test_annotations = example_data.example_annotations(6,:);
 
-% % use the filtered signal as the test signal
-% test_recordings = signal_filtered;
-% % wrap the test signal in a cell
-% test_recordings = {test_recordings};
-% numPCGs = length(test_recordings);
+% use the filtered signal as the test signal
+test_recordings = signal_filtered;
+% wrap the test signal in a cell
+test_recordings = {test_recordings};
+numPCGs = length(test_recordings);
 
-% % Train the HMM:
-% [B_matrix, pi_vector, total_obs_distribution] = trainSpringerSegmentationAlgorithm(train_recordings,train_annotations,springer_options.audio_Fs, false);
+% Train the HMM:
+[B_matrix, pi_vector, total_obs_distribution] = trainSpringerSegmentationAlgorithm(train_recordings,train_annotations,springer_options.audio_Fs, false);
 
-% % Verify the HMM on an unseen test recording: 
-% for PCGi = 1:numPCGs
-%     % [assigned_states] = runSpringerSegmentationAlgorithm(test_recordings{PCGi}, springer_options.audio_Fs, B_matrix, pi_vector, total_obs_distribution, true);
-%     [assigned_states] = runSpringerSegmentationAlgorithm(test_recordings{PCGi}, fs, B_matrix, pi_vector, total_obs_distribution, true);
-% end
+% Verify the HMM on an unseen test recording: 
+for PCGi = 1:numPCGs
+    % [assigned_states] = runSpringerSegmentationAlgorithm(test_recordings{PCGi}, springer_options.audio_Fs, B_matrix, pi_vector, total_obs_distribution, true);
+    [assigned_states] = runSpringerSegmentationAlgorithm(test_recordings{PCGi}, fs, B_matrix, pi_vector, total_obs_distribution, true);
+end
 
 % -------------------------------------------------------------------------
 % 4 feature extraction
